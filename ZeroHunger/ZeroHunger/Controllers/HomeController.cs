300201c24a96828ebfd61b42413300d5aec90530
pial36 <pialhassanchowdhury@gmail.com>
2023-11-23 11:17:56 +0600

    first commit

---
+++ ZeroHunger/ZeroHunger/Controllers/HomeController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZeroHunger.Models;
+
+
+namespace ZeroHunger.Controllers
+{
+    public class HomeController : Controller
+
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+   
+        public ActionResult Employees()
+        {
+            var db = new ZeroHungerEntities();
+            return View(db.Employees.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult AddEmployees()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddEmployees(Employee model)
+        {
+            var db = new ZeroHungerEntities();
+            model.Sex = "Male";
+            db.Employees.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("Employees", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult DeleteEmployee(int id)
+        {
+            var employee = new Employee() { Id = id };
+
+            using (var context = new ZeroHungerEntities())
+            {
+                context.Employees.Attach(employee);
+                context.Employees.Remove(employee);
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("Employees", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateEmployee(int id)
+        {
+            var db = new ZeroHungerEntities();
+            var model = (from employee in db.Employees
+                         where employee.Id == id
+                         select employee).SingleOrDefault();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateEmployee(Employee model)
+        {
+            var db = new ZeroHungerEntities();
+            var employee = db.Employees.SingleOrDefault(e => e.Id == model.Id);
+
+            if (employee != null)
+            {
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Age = model.Age;
+                employee.Sex = "Male";
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Employees", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult AddRestaurants()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddRestaurants(Restaurant restaurant)
+        {
+            var db = new ZeroHungerEntities();
+            db.Restaurants.Add(restaurant);
+            db.SaveChanges();
+            return RedirectToAction("", "Restaurants");
+        }
+
+    }
+}
---
+++ ZeroHunger/ZeroHunger/Controllers/LoginController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using ZeroHunger.Models;
+
+namespace ZeroHunger.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult NGO()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Ngo model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (var db = new ZeroHungerEntities())
+                    {
+                        // Check if the username and password match a record in the 'Ngo' table
+                        var user = db.Ngoes.FirstOrDefault(u => u.username == model.username && u.password == model.password);
+
+                        if (user != null)
+                        {
+                            // Authentication successful, you can store user details in a session or cookie
+                            // and redirect to the home page or wherever you need
+                            return RedirectToAction("Index", "Home");
+                        }
+                        else
+                        {
+                            ViewBag.ErrorMessage = "Invalid username or password.";
+                            return View("NGO");
+                        }
+                    }
+                }
+
+                // Model state is not valid, return to the view with validation errors
+                return View("NGO", model);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception or handle it appropriately
+                ViewBag.ErrorMessage = "An error occurred while processing the login.";
+                return View("Error");
+            }
+        }
+    }
+}
+
+
+  
+
+
